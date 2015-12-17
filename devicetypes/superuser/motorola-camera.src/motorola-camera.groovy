1b2d22b3de20a8d932e3808fa525e72f50dc8551
Big Webstas <jwebstas@gmail.com>
2015-12-17 10:23:51 -0700

    Update motorola-camera.groovy

--- devicetypes/superuser/motorola-camera.src/motorola-camera.groovy
+++ devicetypes/superuser/motorola-camera.src/motorola-camera.groovy
@@ -9,8 +9,11 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
- *
+ * 
+ * The "Take" functionality borrowed from patrick@patrickstuart.com  -  Generic Camera Device v1.0.07102014
+ *  
  */
+ 
 preferences {
     input("ip", "text", title: "IP", description: "Camera IP address", required: true, displayDuringSetup: true)
     } 
