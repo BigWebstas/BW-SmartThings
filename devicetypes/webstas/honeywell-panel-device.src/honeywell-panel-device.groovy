2279bcab820310bf835c02fb30b37e02691a8b2c
Josh Webster <JJWebster@Xactware.com>
2016-02-23 17:25:44 -0700

    Honeywell panel integration - moved code to new device type

--- devicetypes/webstas/honeywell-panel-device.src/honeywell-panel-device.groovy
+++ devicetypes/webstas/honeywell-panel-device.src/honeywell-panel-device.groovy
@@ -13,44 +13,126 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+preferences {
+    input("ip", "text", title: "IP", description: "The IP of your alarmserver")
+    input("port", "text", title: "Port", description: "The port")
+    //input("pincode", "text", title: "Pincode", description: "Alarm Pincode")
+	} 
+// for the UI
 metadata {
-	definition (name: "Honeywell Panel Device", namespace: "Webstas", author: "BigWebstas") {
-		capability "Alarm"
-	}
+  // Automatically generated. Make future change here.
+  	definition (name: "Honeywell Panel Device", namespace: "Webstas", author: "BigWebstas") {
+    // Change or define capabilities here as needed
+    capability "Switch"
+    capability "Refresh"
+    capability "Polling"
 
-	simulator {
-		// TODO: define status and reply messages here
-	}
+    // Add commands as needed
+    command "partition"
+    command "stayarm"
+	command "arm"
+	command "disarm"
+  }
 
-	tiles {
-		// TODO: define your main and details tiles here
-	}
+  tiles (scale: 2){
+    standardTile("dscpartition", "device.dscpartition", width: 6, height: 4, canChangeBackground: true, canChangeIcon: true) {
+      state "armed-stay",     label: 'Armed Stay',      backgroundColor: "#008CC1", icon:"st.Transportation.transportation13"
+      state "armed-away",     label: 'Armed Away',      backgroundColor: "#800000", icon:"st.Transportation.transportation13"
+      state "armed-max",     label: 'Armed MAX',      backgroundColor: "#800000", icon:"st.Transportation.transportation13"
+      state "ready-bypass",     label: 'Ready Bypass',      backgroundColor: "#800000", icon:"st.Transportation.transportation12"
+      state "exit/entry-delay", label: 'Entry/Exit Delay', backgroundColor: "#ff9900", icon:"st.Home.home3"
+      state "notready",  label: 'Not Ready',  backgroundColor: "#ffcc00", icon:"st.Home.home2"
+      state "ready",     label: 'Ready',      backgroundColor: "#79b821", icon:"st.Transportation.transportation12"
+      state "alarm",     label: '!!Alarm!!',      backgroundColor: "#ff0000", icon:"st.Home.home3"
+    }
+
+		standardTile("disarm", "capability.momentary", width: 2, height: 2, title: "Disarm", required: true, multiple: false){
+			state "disarm", label: 'Disarm', action: "disarm", icon: "st.Home.home3", backgroundColor: "#79b821"
+		}
+
+		standardTile("arm", "capability.momentary", width: 2, height: 2, title: "Arm", required: true, multiple: false){
+			state "arm", label: 'Arm - Away', action: "arm", icon: "st.Home.home2", backgroundColor: "#800000"
+       }
+		standardTile("stayarm", "capability.momentary", width: 2, height: 2, title: "Armed Stay", required: true, multiple: false){
+			state "stayarm", label: 'Arm - Stay', action: "stayarm", icon: "st.Home.home4", backgroundColor: "#008CC1"
+       }
+       
+    standardTile("main", "device.dscpartition", width: 6, height: 4, canChangeBackground: true, canChangeIcon: true) {
+      state "armed-stay",     label: 'Armed Stay',      backgroundColor: "#800000", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+  	  state "armed-away",     label: 'Armed Away',      backgroundColor: "#800000", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+  	  state "armed-max",     label: 'Armed MAX',      backgroundColor: "#800000", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+	  state "ready-bypass",     label: 'Ready Bypass',      backgroundColor: "#800000", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+      state "exit/entry-delay",	label: 'Entry/Exit Delay', backgroundColor: "#ff9900", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+      state "notready",  label: 'Not Ready',  backgroundColor: "#ffcc00", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+      state "ready",     label: 'Ready',      backgroundColor: "#79b821", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+      state "alarm",     label: '!!Alarm!!',      backgroundColor: "#ff0000", icon:"http://d1unzhqf5a606m.cloudfront.net/images/large/honeywell-6150-fixed-english-alarm-keypad-with-function-buttons.jpg?1340151882"
+    }
+		main (["main"])	
+		details(["dscpartition", "arm", "stayarm", "staybutton", "disarm"])
+  }
 }
 
 // parse events into attributes
 def parse(String description) {
-	log.debug "Parsing '${description}'"
-	// TODO: handle 'alarm' attribute
+  def myValues = description.tokenize()
 
+  log.debug "Event Parse function: ${description}"
+  sendEvent (name: "${myValues[0]}", value: "${myValues[1]}")
 }
 
-// handle commands
-def off() {
-	log.debug "Executing 'off'"
-	// TODO: handle 'off' command
+def partition(String state, String partition) {
+    log.debug "Partition: ${state} for partition: ${partition}"
+    sendEvent (name: "dscpartition", value: "${state}")
+    if (state != "notready") { //attempt at fixing the duplicate disarm when using the physical panel
+		sendEvent (name: "switch", value: "${state}")
+    } else { sendEvent (name: "switch", value: "ready") }
 }
 
-def strobe() {
-	log.debug "Executing 'strobe'"
-	// TODO: handle 'strobe' command
+def refresh() {
+  poll()
+}
+def stayarm() {
+    def result = new physicalgraph.device.HubAction(
+        method: "GET",
+        path: "/api/alarm/stayarm",
+        headers: [
+            HOST: "$ip:$port"
+        ]
+    )
+    log.debug "response" : "Request to stay arm received"
+    //log.debug "stayarm"
+    sendEvent (name: "switch", value: "stayarm")
+    return result
 }
 
-def siren() {
-	log.debug "Executing 'siren'"
-	// TODO: handle 'siren' command
+def arm() {
+    def result = new physicalgraph.device.HubAction(
+        method: "GET",
+        path: "/api/alarm/arm",
+        headers: [
+            HOST: "$ip:$port"
+        ]
+    )
+    log.debug "response" : "Request to arm received"
+    //log.debug "arm"
+    sendEvent (name: "switch", value: "arm")
+    return result
 }
 
-def both() {
-	log.debug "Executing 'both'"
-	// TODO: handle 'both' command
+def disarm() {
+    def result = new physicalgraph.device.HubAction(
+        method: "GET",
+        path: "/api/alarm/disarm",
+        headers: [
+            HOST: "$ip:$port"
+        ]
+    )
+    log.debug "response" : "Request to disarm received"
+    //log.debug "disarm"
+    sendEvent (name: "switch", value: "disarm")
+    return result
+}
+
+def poll() {
+	log.debug "Executing 'poll'"
 }
